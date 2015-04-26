b9f25e20d0a90a9e9d67838cf9838defa9d65815
chengnaixue <chengnaixue@gmail.com>
2015-04-26 14:29:48 +0800

    add new file

--- main/test.go
+++ main/test.go
@@ -1,5 +1,48 @@
 package main
-import "fmt"
-func main(){
-fmt.Printf("Hello,world\n")
+
+import "fmt" //alias
+import "os"
+
+var a string
+
+func main() {
+
+ 	str := "Go says hello to the world!"
+
+	var (
+		HOME = os.Getenv("HOME")
+		USER = os.Getenv("USER")
+		GOROOT = os.Getenv("GOROOT")
+	)
+
+	fmt.Printf("Hello,world\n")
+
+	fmt.Println("Hello,world")
+
+	fmt.Printf("Καλημέρα κόσμε; or こんにちは 世界\n")
+
+	fmt.Println(&str);
+
+	fmt.Println(HOME)
+	fmt.Println(USER)
+	fmt.Println(GOROOT)
+
+	fmt.Printf("The operationg system user is: %s\n", USER)
+
+	a = "G"
+	print(a)
+	f1()
+
 }
+
+func f1(){
+	a := "O"
+	print(a)
+	f2()
+}
+
+func f2(){
+	print(a)
+}
+
+
